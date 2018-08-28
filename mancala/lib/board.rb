0bab7cc7845704ac56061300d985e2c649b50240
amanpriya-k <amanpriya.kulkarni@gmail.com>
2018-08-27 22:40:07 -0700

    Finish every method in Mancala except make_move

---
+++ mancala/lib/board.rb
@@ -0,0 +1,55 @@
+class Board
+  attr_accessor :cups
+
+  def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) { Array.new }
+    place_stones
+  end
+
+  def place_stones
+    # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, i|
+      if i!=6 && i!=13
+        @cups[i] += [:stone, :stone, :stone, :stone]
+      end
+    end
+  end
+
+  def valid_move?(start_pos)
+    raise 'Invalid starting cup' if start_pos < 0 || start_pos > 12
+    raise 'Starting cup is empty'   if @cups[start_pos].length == 0
+  end
+
+  def make_move(start_pos, current_player_name)
+    
+  end
+
+  def next_turn(ending_cup_idx)
+    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    return :prompt if ending_cup_idx == 6 || ending_cup_idx == 13
+    return :switch if @cups[ending_cup_idx].length-1 == 0 #cup empty before ending?
+    ending_cup_idx
+  end
+
+  def render
+    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    puts ""
+    puts ""
+  end
+
+  def one_side_empty?
+    @cups[0..5].all? { |cup| cup.length == 0 } || @cups[7..12].all? { |cup| cup.length == 0 }
+  end
+
+  def winner
+    score1 = @cups[6].length
+    score2 = @cups[13].length
+    return :draw if score1 == score2
+    return @name1 if score1 > score2
+    @name2
+  end
+end
