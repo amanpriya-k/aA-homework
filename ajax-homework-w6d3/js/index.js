bce76ea4390ba7d737d7e7e820365b76cbc975fc
amanpriya-k <amanpriya.kulkarni@gmail.com>
2018-09-25 17:55:23 -0700

    Finish AJAX homework

--- ajax-homework-w6d3/js/index.js
+++ ajax-homework-w6d3/js/index.js
@@ -1,5 +1,15 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
+$.ajax( {
+  url: 'http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b',
+  type: 'GET',
+  success(weatherData) {
+    console.log(`Your weather data:`);
+    console.log(`temperature: ${weatherData.main.temp}, humidity: ${weatherData.main.humidity}`);
+    console.log(`description: ${weatherData.weather[0].description}`);
+  }
+});
 
 // Add another console log here, outside your AJAX request
+console.log("Finished AJAX request!");
